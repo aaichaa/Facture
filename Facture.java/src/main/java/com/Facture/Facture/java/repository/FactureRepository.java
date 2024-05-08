a7560d09cffba0afd7843ff7d3168821f75f220f
Aissatou Bobo <abd2004bobo@gmail.com>
2024-05-08 18:43:33 +0200

    update

--- Facture.java/src/main/java/com/Facture/Facture/java/repository/FactureRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Facture.Facture.java.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.Facture.Facture.java.model.Facture;
-@Repository
-public interface FactureRepository extends JpaRepository<Facture, Integer> {
-}
